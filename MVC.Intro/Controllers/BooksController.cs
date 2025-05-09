c2c974157c6c6175aa1608bd435c5bb5f454a999
‪Hany Abdou Saad‬‏ <Hanysaadstd@gmail.com>
2025-05-09 23:52:08 +0300

    first commit

---
+++ MVC.Intro/Controllers/BooksController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Intro.Models;
+
+namespace MVC.Intro.Controllers;
+
+public class BooksController : Controller
+{
+    private static List<Book> _books = Book.GetBooksList();
+    private readonly IConfiguration _configuration;
+
+    public BooksController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(_books);// Views/Books/Index.cshtml
+    }
+
+    [HttpGet]
+    public IActionResult Details(Guid id)
+    {
+        var book = _books.FirstOrDefault(b => b.Id == id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        return View(book);// Views/Books/Details.cshtml
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();// Views/Books/Add.cshtml
+    }
+
+    [HttpPost]
+    public IActionResult Add(AddBookDto bookDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(bookDto);
+        }
+
+        // Logic to add book to db
+        return RedirectToAction("Index");
+    }
+}
---
+++ MVC.Intro/Models/AddBookDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Intro.Models;
+
+public class AddBookDto
+{
+    [Required]
+    [StringLength(6)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(10, MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    // suitable input is <input type="password"/>
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = string.Empty;
+}
